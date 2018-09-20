22497756f00ef82a965585ac5d0c56726a6e768d
遊歩 <yuho.ito@walker.co.jp>
2018-09-20 12:21:02 +0900

    streamingcamera 追加

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Camera from './Camera';
 import RNCamera from './RNCamera';
+import StreamingCamera from './StreamingCamera';
 import FaceDetector from './FaceDetector';
 
 export { RNCamera, FaceDetector };
